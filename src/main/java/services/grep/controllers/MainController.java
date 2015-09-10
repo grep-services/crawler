0c752effaf5cdcf9f68297d36f93ff92a0963520
marine <marine@grep.services>
2015-09-10 13:13:22 +0900

    execute mode strategies modified

--- src/main/java/services/grep/controllers/MainController.java
+++ src/main/java/services/grep/controllers/MainController.java
@@ -12,7 +12,8 @@ import main.java.services.grep.utils.MultiPrinter;
  * 
  * Account 받아 Task에 줘서 필요한 Query 실행시키고 DB에까지 기록.
  * Input을 받아 Account를 관리하거나, Task를 실행/중지 시키거나, DB의 내용을 확인하는 등의 관리.
- * 세부적인 전략으로는, Crawl은 사용자의 명령에 의해 실행되고, 추가분에 대한 Update는 1회/1일, 삭제분에 대해서는 1회/1주 로 한다.
+ * refresh 1일단위, 삭제확인 7일단위, 수정확인 1달단위로 간다.
+ * 각 module은 이 package에 담아두고 이 class에서 가져와 사용한다.
  * 
  * 당장은 몰라도, 앞으로는 Daemon으로 작동할 수 있도록 만든다.
  * 타 object들도 일단 가능한 대로 singleton보다 단순 instance 방식으로 간다.
--- src/main/java/services/grep/utils/Constants.java
+++ src/main/java/services/grep/utils/Constants.java
@@ -4,6 +4,8 @@ package main.java.services.grep.utils;
  * 
  * 기본적인 것은 각자 쓰되, global한 것은 여기서 쓴다.
  * 
+ * refresh, 수정, 삭제 등의 mode를 변경하거나 하려면, singleton으로 바꿔야 될지도 모를듯...
+ * 
  * @author marine1079
  * @since 150714
  *
